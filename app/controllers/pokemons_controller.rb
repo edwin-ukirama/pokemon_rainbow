947b53fba0b234e9ed9686e823cae7505ed40ac4
Edwin Leonard Salim <edwin.salim@ukirama.com>
2022-10-05 16:18:42 +0700

    Add pokemon skills feature

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -6,6 +6,8 @@ class PokemonsController < ApplicationController
 
   def show
     @pokemon = Pokemon.find(params[:id])
+    @skills = Skill.where(element_type: [@pokemon.pokedex.element_type, "normal"])
+          .where.not(id: @pokemon.skills.ids)
   end
 
   def new
@@ -62,7 +64,59 @@ class PokemonsController < ApplicationController
   def heal
     @pokemon = Pokemon.find(params[:pokemon_id])
     @pokemon.current_health_point = @pokemon.max_health_point
+    @pokemon.save
+
+    flash[:success] = "#{@pokemon.name} has been healed!"
+
+    redirect_to @pokemon
   end
+
+  def add_skill
+      @pokemon = Pokemon.find(params[:pokemon_id])
+      @skill = Skill.find(params[:skill_id])
+      @pokemon_skill = PokemonSkill.new
+      @pokemon_skill.pokemon_id = @pokemon.id
+      @pokemon_skill.skill_id = @skill.id
+      @pokemon_skill.current_pp = @skill.max_pp
+
+      if @pokemon_skill.save
+        flash[:success] = "#{@skill.name} added!"
+      else
+        flash[:error] = "#{@pokemon_skill.errors}"
+      end
+
+      redirect_to @pokemon
+  end
+
+  def refill_skill_pp
+    @pokemon = Pokemon.find(params[:pokemon_id])
+    @pokemon_skill = @pokemon.pokemon_skills.find(params[:skill_id])
+    @pokemon_skill.current_pp = @pokemon_skill.skill.max_pp
+
+    if @pokemon_skill.save
+      flash[:success] = "#{@pokemon_skill.skill.name.titleize} pp has been refilled!"
+      redirect_to edit_pokemon_path(@pokemon)
+    else
+      flash[:error] = "#{@pokemon_skill.errors}"
+      redirect_to edit_pokemon_path(@pokemon), status: :unprocessable_entity
+    end
+  end
+
+  def remove_skill
+    @pokemon = Pokemon.find(params[:pokemon_id])
+    @pokemon_skill = @pokemon.pokemon_skills.find(params[:skill_id])
+    @pokemon_skill.destroy
+
+    if @pokemon_skill.destroyed?
+      flash[:success] = "#{@pokemon_skill.skill.name.titleize} has been removed!"
+      redirect_to edit_pokemon_path(@pokemon)
+    else
+      flash[:error] = "#{@pokemon_skill.errors}"
+      redirect_to edit_pokemon_path(@pokemon), status: :unprocessable_entity
+    end
+  end
+
+
 
   private
   def pokemon_params
