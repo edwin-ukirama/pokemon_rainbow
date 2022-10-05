6c57b78c2c9e30c06de365786a321d219860a64c
Edwin Leonard Salim <edwin.salim@ukirama.com>
2022-10-05 13:19:53 +0700

    Add validations to Pokedex model

--- app/models/pokedex.rb
+++ app/models/pokedex.rb
@@ -21,10 +21,10 @@ class Pokedex < ApplicationRecord
   ]
 
   validates :name, presence: true, uniqueness: true
-  validates :health_point, presence: true
-  validates :base_attack, presence: true
-  validates :base_defense, presence: true
-  validates :base_speed, presence: true
+  validates :health_point, numericality: { greater_than: 0}
+  validates :base_attack, numericality: { greater_than: 0}
+  validates :base_defense, numericality: { greater_than: 0}
+  validates :base_speed, numericality: { greater_than: 0}
   validates :element_type, inclusion: { in: TYPES }, presence:true
   validates :image_url, presence: true, allow_blank: true
   validates :image_url, format: { with: URI.regexp, message: 'must be an url' }
